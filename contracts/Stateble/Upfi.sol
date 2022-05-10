ef142fe066c664e8dcf3a7708c0ce44ec720434b
Tim1997 <timbkhn@gmail.com>
2022-05-10 23:37:55 +0700

    ...

--- contracts/Stateble/Upfi.sol
+++ contracts/Stateble/Upfi.sol
@@ -2,19 +2,42 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
 contract Upfi {
     using Math for uint256;
     using SafeMath for uint256;
-    mapping(address => uint256) public minters;
+    mapping(address => uint256) internal balances;
+    address owner;
 
-    modifier onlyMinter() {
-        require(minters[msg.sender] > 0, "Only minter can interact");
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can request.");
         _;
     }
 
-    function mint() external payable {}
+    error InsufficientBalance(uint256 requested, uint256 available);
 
-    function redeem() external payable {}
+    function getBalance() public view returns (uint256) {
+        return ERC20(owner).balanceOf(msg.sender);
+    }
+
+    function mint(uint256 amount) external payable onlyOwner {
+        if (amount > getBalance())
+            revert InsufficientBalance({
+                requested: amount,
+                available: getBalance()
+            });
+
+        balances[msg.sender] = balances[msg.sender].sub(amount);
+        // ERC20(owner).
+    }
+    // làm thế nào để chuyển 1 giá trị coin UPFI vào ví người dùng?
+    // làm thế nào để trừ đi giá trị các token người đó đang sở hữu
+    // làm thế nào để biết các giá trị các token đang ở trong wallet của họ
+    function redeem() external payable onlyOwner {}
 }
